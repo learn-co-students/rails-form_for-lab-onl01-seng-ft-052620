2e32f7446d966130c0bb0bf658a745cfe40386e2
naechrist <729jenae.christensen@gmail.com>
2020-08-03 15:02:53 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,16 +9,24 @@ class StudentsController < ApplicationController
     end 
 
     def create
-        @student = Student.new(params.require(:student).permit(:first_name, :last_name))
+        @student = Student.new(student_params)
         @student.save 
         redirect_to student_path(@student)
     end 
 
     def edit 
         @student = Student.find(params[:id])
-        # binding.pry
-        @student.update(params.require(:student).permit(:first_name, :last_name))
-        
+    end 
+
+    def update 
+        @student = Student.find(params[:id])
+        @student.update(student_params)
         redirect_to student_path(@student)
+    end 
+
+    private
+
+    def student_params
+        params.require(:student).permit!
     end 
 end 
