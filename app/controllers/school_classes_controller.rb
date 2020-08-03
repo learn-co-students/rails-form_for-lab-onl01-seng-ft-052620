de45cf0075765531ddfa1e9d6953c835cde76279
naechrist <729jenae.christensen@gmail.com>
2020-08-03 14:36:03 -0400

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,7 +1,7 @@
 class SchoolClassesController < ApplicationController 
 
     def create
-        @school_class = SchoolClass.new(params.require(:school_class))
+        @school_class = SchoolClass.new(params.require(:school_class).permit(:title, :room_number))
         @school_class.save
         redirect_to school_class_path(@school_class)
     end
@@ -11,8 +11,16 @@ class SchoolClassesController < ApplicationController
     end 
     
     def show
+        @school_class = SchoolClass.find(params[:id])
     end
 
     def edit 
+        @school_class = SchoolClass.find(params[:id])
+    end 
+
+    def update 
+        @school_class = SchoolClass.find(params[:id])
+        @school_class.update(params.require(:school_class).permit(:title, :room_number))
+        redirect_to school_class_path(@school_class)
     end 
 end 
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,17 +1,24 @@
 class StudentsController < ApplicationController
 
-    def index
-    end 
-    
+    def show
+        @student = Student.find(params[:id])
+    end
+        
     def new
+        @student = Student.new
     end 
 
     def create
+        @student = Student.new(params.require(:student).permit(:first_name, :last_name))
+        @student.save 
+        redirect_to student_path(@student)
     end 
-    
-    def show
-    end
 
     def edit 
+        @student = Student.find(params[:id])
+        # binding.pry
+        @student.update(params.require(:student).permit(:first_name, :last_name))
+        
+        redirect_to student_path(@student)
     end 
 end 
